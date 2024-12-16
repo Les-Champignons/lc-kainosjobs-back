e576be12dc1b405269fed276f556b9a298e8cc12
CharlotteMKainos <charlotte.mcknight@kainos.com>
2024-12-16 16:19:12 +0000

    swagger GET endpoint getalljobroles created

--- src/main/java/org/kainos/ea/KainosJobsApplication.java
+++ src/main/java/org/kainos/ea/KainosJobsApplication.java
@@ -6,6 +6,9 @@ import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 import io.dropwizard.Application;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
+import org.kainos.ea.controllers.JobRoleController;
+import org.kainos.ea.dao.JobRoleDao;
+import org.kainos.ea.services.JobRoleService;
 
 
 public class KainosJobsApplication extends Application<KainosJobsConfiguration> {
@@ -36,7 +39,16 @@ public class KainosJobsApplication extends Application<KainosJobsConfiguration> 
     @Override
     public void run(final KainosJobsConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+
+        environment.jersey()
+                .register(
+                        new JobRoleController(
+                                new JobRoleService(
+                                        new JobRoleDao()
+                                )
+                        )
+                );
     }
+
 
 }
--- src/main/java/org/kainos/ea/controllers/JobRoleController.java
+++ src/main/java/org/kainos/ea/controllers/JobRoleController.java
@@ -1,6 +1,36 @@
 package org.kainos.ea.controllers;
 import io.swagger.annotations.Api;
+import org.kainos.ea.exceptions.DoesNotExistException;
+import org.kainos.ea.services.JobRoleService;
 
-@Api()
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+
+@Api("Kainos Jobs")
+@Path("/api/job-roles")
 public class JobRoleController {
+    private final JobRoleService jobRoleService;
+
+    public JobRoleController(
+            JobRoleService jobRoleService
+    ) {
+        this.jobRoleService = jobRoleService;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllJobRoles() {
+        try {
+            return Response.ok().entity(jobRoleService.getAllJobRoles()).build();
+        } catch (SQLException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(e.getMessage()).build();
+        }
+
+    }
 }
+
